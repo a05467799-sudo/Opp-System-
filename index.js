8bd74611181d50a8640c8c1977da3315a8a8602b
a05467799-sudo <a05467799@gmail.com>
2026-06-24 03:35:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+require('dotenv').config();
+
+const {
+    Client,
+    GatewayIntentBits,
+    EmbedBuilder
+} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+const WELCOME_CHANNEL = '1342954295603105874';
+const AUTO_ROLE = '1342955328379031612';
+
+const WELCOME_IMAGE = 'https://cdn.discordapp.com/attachments/1435630247457460234/1515716454840799333/standard_2.gif';
+
+client.once('clientReady', () => {
+    console.log(`${client.user.tag} Online`);
+});
+
+client.on('guildMemberAdd', async (member) => {
+
+    try {
+
+        console.log(`📥 New member: ${member.user.tag}`);
+
+        const role = await member.guild.roles.fetch(AUTO_ROLE).catch(() => null);
+
+        if (!role) {
+            console.log('❌ Role not found');
+        } else {
+
+            await member.roles.add(role).catch(err => {
+                console.log('❌ Failed to add role:', err);
+            });
+
+            console.log(`✅ Role added to ${member.user.tag}`);
+        }
+
+        const channel = await member.guild.channels.fetch(WELCOME_CHANNEL).catch(() => null);
+
+        if (!channel) {
+            console.log('❌ Welcome channel not found');
+            return;
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor('#1E1F22')
+            .setAuthor({
+                name: member.user.username,
+                iconURL: member.user.displayAvatarURL({ size: 1024 })
+            })
+            .setTitle('Welcome to [DEVION STORE]')
+            .setDescription(`Welcome ${member} to the server 👋`)
+            .addFields(
+                {
+                    name: '👤 Member',
+                    value: `${member}`,
+                    inline: true
+                },
+                {
+                    name: '📅 Account Age',
+                    value: `<t:${Math.floor(member.user.createdTimestamp / 1000)}:R>`,
+                    inline: true
+                },
+                {
+                    name: '👥 Members Count',
+                    value: `${member.guild.memberCount}`,
+                    inline: true
+                }
+            )
+            .setThumbnail(member.user.displayAvatarURL({ size: 1024 }))
+            .setImage(WELCOME_IMAGE)
+            .setTimestamp()
+            .setFooter({
+                text: 'Developed by NOS Store',
+                iconURL: member.guild.iconURL()
+            });
+
+        await channel.send({
+            content: `${member}`,
+            embeds: [embed]
+        });
+
+    } catch (err) {
+        console.log('❌ Error in guildMemberAdd:', err);
+    }
+
+});
+
+client.login(process.env.TOKEN);
